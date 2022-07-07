f40aa04c4fca09bd7b9e074ac20cadffe74192f1
Magnus Brantheim <m.brantheim@gmail.com>
2022-07-07 14:33:19 +0200

    Upgrade ETH tarot strategy

--- scripts/upgradeProxy.js
+++ scripts/upgradeProxy.js
@@ -1,7 +1,6 @@
 async function main() {
-  const stratFactory = await ethers.getContractFactory('ReaperAutoCompoundSolidexFarmer');
-  const stratContract = await hre.upgrades.upgradeProxy('0x3630a380F320EA77284Ed03D09B4C73D1351C41e', stratFactory, {
-    call: {fn: 'postUpgradeLP0Allowance'},
+  const stratFactory = await ethers.getContractFactory('ReaperStrategyTarot');
+  const stratContract = await hre.upgrades.upgradeProxy('0x58907Ac386dB688860125bdB035Ae24505fA28e4', stratFactory, {
     timeout: 0,
   });
   console.log('Strategy upgraded!');
